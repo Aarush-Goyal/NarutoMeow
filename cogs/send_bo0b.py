a4aa20da65c4a33bbdac502a5d1182c1c15511b4
Aarush-Goyal <goyalaarush92@gmail.com>
2021-02-23 04:16:15 +0530

    updated send_bo0b commmand

--- cogs/send_bo0b.py
+++ cogs/send_bo0b.py
@@ -4,16 +4,16 @@ from discord.ext import commands
 # from main import WELCOME_MESSAGE_CHANNEL_ID
 
 
-class SendBob(commands.Cog):
+class SendBo0b(commands.Cog):
 
     def __init__(self, client):
         self.client = client
 
     # Commands
-    @ commands.command(brief="Sends bo*b")
-    async def send_bob(self, context, *args):
+    @ commands.command(brief="Sends bo0b")
+    async def send_bo0b(self, context, *args):
         await context.message.channel.send(f'https://gph.is/g/EGyyPx1')
 
 
 def setup(client):
-    client.add_cog(SendBob(client))
+    client.add_cog(SendBo0b(client))
