4b4d9f70496b93a07f7337cf87b05b0321305179
Aarush-Goyal <goyalaarush92@gmail.com>
2021-02-22 23:14:44 +0530

    added who_am_i command

---
+++ cogs/who_am_i.py
@@ -0,0 +1,19 @@
+import discord
+from discord import client
+from discord.ext import commands
+# from main import WELCOME_MESSAGE_CHANNEL_ID
+
+
+class WhoAmI(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    # Commands
+    @ commands.command(brief="Tells the person their username")
+    async def who_am_i(self, context, *args):
+        await context.message.channel.send(f'You are @{context.message.author}')
+
+
+def setup(client):
+    client.add_cog(WhoAmI(client))
