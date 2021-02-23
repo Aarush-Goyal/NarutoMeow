2d52fafa25543255b74534e248f4cbceab567077
Aarush-Goyal <goyalaarush92@gmail.com>
2021-02-23 09:40:01 +0530

    added random command

---
+++ cogs/random.py
@@ -0,0 +1,27 @@
+import typing
+import discord
+from discord import client
+from discord.ext import commands
+import aiohttp
+
+
+class Random(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    # Commands
+    @commands.command(name="random", brief="Sends random cat and dog embeds")
+    async def random(self, context, *args):
+        if("cat" in args):
+            async with context.message.channel.typing():
+                async with aiohttp.ClientSession() as cs:
+                    async with cs.get("http://aws.random.cat/meow") as r:
+                        data = await r.json()
+                        embed = discord.Embed(title="Meow")
+                        embed.set_image(url=data['file'])
+                        await context.send(embed=embed)
+
+
+def setup(client):
+    client.add_cog(Random(client))
